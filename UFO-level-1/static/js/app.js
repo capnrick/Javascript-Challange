7ea7284c2e5f0c5d013a7421f3d9b548ef400d30
capnrick <herrric@hawk.iit.edu>
2022-01-15 23:06:46 -0600

    Adding code for app.js for adding data from data.js into a table to display in index.html.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,18 @@
 // from data.js
 var tableData = data;
 
+//get handle on the tbody element for use in the function below
+var tbody=d3.select("tbody");
+
 // YOUR CODE HERE!
+data.forEach(function(ufoReport) {
+    console.log(ufoReport);
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the ufo report object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
